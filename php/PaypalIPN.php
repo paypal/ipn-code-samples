af8f6eb33a426c9e7ecbb5e67e8a28e25453bf50
overint <emcmahonzx@gmail.com>
2016-09-28 12:01:38 +1000

    fix TLS 1.2

--- php/PaypalIPN.php
+++ php/PaypalIPN.php
@@ -107,7 +107,7 @@ class PaypalIPN
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
-        curl_setopt($ch, CURLOPT_SSLVERSION, 4);
+        curl_setopt($ch, CURLOPT_SSLVERSION, 6);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 		
