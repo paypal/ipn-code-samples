11277e79b305376d61cef09535f17600f269f19c
Noah Ziems <noah.ziems@live.com>
2016-12-27 12:52:05 -0500

    Response improvement
    The response to PayPal is now done before the information is processed locally to keep paypal from wasting resources by sending more POSTs than is nescessary

--- php/example_usage.php
+++ php/example_usage.php
@@ -7,6 +7,11 @@ $ipn = new PaypalIPN();
 // Use the sandbox endpoint during testing.
 $ipn->useSandbox();
 $verified = $ipn->verifyIPN();
+
+// Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
+// Reply as soon as possible to keep Paypal from sending more POSTs than nescessary
+header("HTTP/1.1 200 OK");
+
 if ($verified) {
     /*
      * Process IPN
@@ -14,6 +19,3 @@ if ($verified) {
      * https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNandPDTVariables/
      */
 }
-
-// Reply with an empty 200 response to indicate to paypal the IPN was received correctly.
-header("HTTP/1.1 200 OK");
