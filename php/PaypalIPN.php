f8b1a1ada6fa091d123581af66ef27354eb8a1b1
BigRedBot <BigRedBot@users.noreply.github.com>
2017-02-17 23:02:09 -0500

    will now always use sandbox uri if test_ipn is set
    will now always use sandbox uri if test_ipn is set

--- php/PaypalIPN.php
+++ php/PaypalIPN.php
@@ -51,7 +51,7 @@ class PaypalIPN
      */
     public function getPaypalUri()
     {
-        if ($this->use_sandbox) {
+        if (($this->use_sandbox) || ($_POST["test_ipn"] == 1)) {
             return self::SANDBOX_VERIFY_URI;
         } else {
             return self::VERIFY_URI;
