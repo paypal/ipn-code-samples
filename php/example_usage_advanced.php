7bd921d6a462d51bb5bec9e19a2ab4ca220c013a
BigRedBot <BigRedBot@users.noreply.github.com>
2017-04-25 12:53:02 -0400

    Update example_usage_advanced.php

--- php/example_usage_advanced.php
+++ php/example_usage_advanced.php
@@ -1,6 +1,4 @@
 <?php
-namespace Listener;
-require('PaypalIPN.php');
 
 // Set this to true to use the sandbox endpoint during testing:
 $enable_sandbox = true;
@@ -22,6 +20,8 @@ $log_file_dir = __DIR__ . "/logs";
 
 
 
+namespace Listener;
+require('PaypalIPN.php');
 use PaypalIPN;
 $ipn = new PaypalIPN();
 if ($enable_sandbox) {
