f2dab2455fcb6ea733913f09726455ac525725e3
Tim Smith <tzs@tzs.net>
2018-07-24 16:53:53 -0700

    Update PHP to allow caller to set curl options
    Many production environments restrict outgoing network access on their servers that deal with payments, only allowing such access through proxies that vet the destinations before allowing access. This patch provides an option for the caller of verifyIPN() to provide the curl object for verifyIPN() to use instead of allocating its own curl object. This allows the caller to set any proxy options, or other options, that may be required in order to allow the connection back to Paypal to work.

--- php/PaypalIPN.php
+++ php/PaypalIPN.php
@@ -6,6 +6,8 @@ class PaypalIPN
     private $use_sandbox = false;
     /** @var bool Indicates if the local certificates are used. */
     private $use_local_certs = true;
+    /** @var curl handler */
+    private $curl_handler = null;
 
     /** Production Postback URL */
     const VERIFY_URI = 'https://ipnpb.paypal.com/cgi-bin/webscr';
@@ -16,6 +18,11 @@ class PaypalIPN
     const VALID = 'VERIFIED';
     /** Response from PayPal indicating validation failed */
     const INVALID = 'INVALID';
+
+    public function __construct($curl_handler = null)
+    {
+        $this->curl_handler = $curl_handler;
+    }
 
     /**
      * Sets the IPN verification to sandbox mode (for use when testing,
@@ -96,7 +103,8 @@ class PaypalIPN
         }
 
         // Post the data back to PayPal, using curl. Throw exceptions if errors occur.
-        $ch = curl_init($this->getPaypalUri());
+        $ch = isset($this->curl_handler) ? $this->curl_handler : curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->getPaypalUri());
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
